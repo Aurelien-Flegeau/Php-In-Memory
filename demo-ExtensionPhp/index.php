24157d92c4fce7ec126ce26effd82c944fc213a1
my account <my account>
2016-05-03 10:19:22 +0200

    lien vm

--- demo-ExtensionPhp/index.php
+++ demo-ExtensionPhp/index.php
@@ -2,10 +2,10 @@
 
 include('mem.php');
 
-echo $data->getData();
+echo $data->getData(); //affiche 0
 
 echo '<br/>';
-$data->setData(2);
-echo $data->getData();
+$data->setData(2);	//met 2
+echo $data->getData(); //affiche 2
 
 ?>
