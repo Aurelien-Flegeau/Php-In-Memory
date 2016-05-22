40b9b6cd7e9ef244e5078143ab9dd67d3c1e52c5
Ismaïl <nguyen.ismail@gmail.com>
2016-05-22 20:31:08 +0200

    Update Data class with string property instead integer

--- demo-ExtensionPhp/data.h
+++ demo-ExtensionPhp/data.h
@@ -1,13 +1,20 @@
 #ifndef DATA_H
 #define DATA_H
- 
-// A very simple car class
-class Data {
-public:
-    Data();
-    int getData();
-    void setData(int value);
-    int m_value;
-};
+
+#include <string>
+using namespace std;
+
+namespace PhpInMemory {
+	class Data {
+		
+		string m_value;
+		
+	public:
+		Data();
+		
+		int getData();
+		void setData(string);
+	};
+}
  
 #endif /* DATA_H */
